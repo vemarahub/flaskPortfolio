c021b2d8c3e8c304550249fb001005f819576167
Rajesh S Nair <67047853+vemarahub@users.noreply.github.com>
2024-06-11 02:07:36 +0200

    Delete Portfolio directory

--- Portfolio/static/js/header.js
+++
@@ -1,7 +0,0 @@
-// include-header.js
-fetch('/static/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching header:', error));
